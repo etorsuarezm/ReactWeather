13d9b568e128890d79ad69700ddc2e1d2cd5b69e
Hector Suarez <etorsuarez@gmail.com>
2017-02-12 15:59:16 -0600

    Style avout page and link to resources

--- app/components/About.js
+++ app/components/About.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import Gravatar from 'react-gravatar';
 
 const About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page</p>
+            <h1 className="text-center">About</h1>
+            <p>Hey I've made this Weather App with React and Foundation for UI styling.</p>            
+            <h2 className="text-center">Who am I ?</h2>
+            <div className="text-center">
+                <Gravatar email="etorsuarez@gmail.com" size={150} />
+            </div>
+            <br/>
+            <br/>
+            <p>Birth developer, JS by choice and React/React Native by passion</p>
+            <h2 className="text-center">Tools I used:</h2>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>- JS Framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a>- Free access Weather API I used
+                    to get weather data by city name.
+                </li>
+                <li>
+                    <a href="http://foundation.zurb.com/">Foundation</a>- UI Framework.
+                </li>
+            </ul>
+
         </div>
         
     );
