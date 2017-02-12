ab773f86db46089ef975881e553d0608db7f59c9
Hector Suarez <etorsuarez@gmail.com>
2017-02-12 15:30:00 -0600

    Example UI implementation

--- app/components/Examples.js
+++ app/components/Examples.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 const Examples = (props) => {
         return (  
             <div>
-                <h3>Examples</h3>
-                <p>Welcome to examples page!</p>
+                <h1 className="text-center">Examples</h1>
+                <p>Here are a few example locations to try out</p>
+                <ol>
+                    <li>
+                        <Link to="/?location=Leon,Gto">León, Gto.</Link>
+                    </li>
+                    <li>
+                        <Link to="/?location=Punta Cana">Punta Cana</Link>
+                    </li>
+                </ol>
             </div>
             
         );
