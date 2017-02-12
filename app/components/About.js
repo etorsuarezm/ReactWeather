6c942567af540e5579814e2d8511cc6c39bffd15
Hector Suarez <etorsuarez@gmail.com>
2017-02-12 02:39:38 -0600

    Remove "Component" string to About title and added paragraph

--- app/components/About.js
+++ app/components/About.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 const About = (props) => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h3>About</h3>
+            <p>Welcome to the About page</p>
+        </div>
+        
     );
 }
 
