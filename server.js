00ea8cd0383b2e6c5b21f0bc17a7e0f502e4e229
Hector Suarez <etorsuarez@gmail.com>
2017-02-12 02:31:24 -0600

    Fix middleware for localhost

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) { // Middleware
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url); // Because open weather API
+    } else {
+        next();
     }
 });
 
